878e9f87ec954d5866765dc39e2bf1126726107b
Felipe Marques Batista <felipefmb@gmail.com>
2019-08-24 13:50:11 -0300

    Criados módulos shared, core. Criada página de login e alterada a home

---
+++ src/app/auth/pages/login/login.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss']
+})
+export class LoginPage implements OnInit {
+  authForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  private createForm(): void {
+    this.authForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get email(): FormControl {
+    
+  }
+
+  onSubmit(): void {
+    console.log('AuthForm: ', this.authForm.value);
+  }
+}
